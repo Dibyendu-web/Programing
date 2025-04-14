3e0a6f197d3ab331e8db0fb239686e3d5fd722df
Dibyendu-web <rabinghosh52@gmail.com>
2025-04-14 16:51:16 +0530

    14042025

--- Julia/Test_Scripts/test.jl
+++ Julia/Test_Scripts/test.jl
@@ -1,148 +1,26 @@
-using QuadGK
-
-function summ(x)
-    return x^2 + 2x    
-end
-
-@time quadgk(x -> summ(x), 0, 10)
-
-using SymPy
-
-x = symbols("x")
-
-y =  x^4 + 5x + 4
-
-s = solve(y, x)
-
-s[2]
-
-using oneAPI 
-
-oneAPI.devices()
-
-oneAPI.device()
-
-include("module test.jl")
-using .test
-test1
-test1()
-
-for x in 1:10, y in 1:10
-    println(x, y)
-end
-
-
-mutable struct person
-    name::String
-    age::Int
-
-    function person()
-        new("John", 20)
-        
-    end
-end
-
-p = person()
-
-include("module test.jl")
-
-using .geometry 
-p = Point(1, 2)
-c = circle(p, 3)
-area(c)
-
-mutable struct tst 
-    const x 
-    y 
-
-end
-
-t = tst(1, 2)
-t.y = 3
-
 using BenchmarkTools
-
-@benchmark begin
-    sum(x -> x^2 + 2x, 1:1000)
+function f1(x::Matrix{Float64}, y::Matrix{Float64})
+    s = size(x)
+    a = zeros(Float64, s)
+    for i in 1:s[1]
+        for j in 1:s[2]
+            a[i, j] = x[i, j] + y[i, j]
+        end
+    end
+    return a
+end
+function f2(x::Matrix{Float64}, y::Matrix{Float64})
+    s = size(x)
+    a = zeros(Float64, s)
+    for i in 1:s[1]
+        for j in 1:s[2]
+            @inbounds a[i, j] = x[i, j] + y[i, j]
+        end
+    end
+    return a
 end
 
-a = Vector{Int}(undef, 1000)
-
-a[5] = 345
-
-function f(x)
-    return x^4 + 5x^3 + 3x^2 + 2x + 1
-end
-
-@show f(2)
-
-@benchmark begin
-    f(2)
-end
-
-# Define a meshgrid function using list comprehensions
-function meshgrid(x::AbstractVector, y::AbstractVector)
-    X = [xi for yi in y, xi in x]
-    Y = [yi for yi in y, xi in x]
-    return X, Y
-end
-
-# Example usage:
-x = LinRange(0, 1, 100)  # e.g., 5 points from 0 to 1
-y = LinRange(0, 1, 100)  # e.g., 3 points from 0 to 1
-
-X, Y = meshgrid(x, y)
-
-println("X grid:")
-println(X)
-println("\nY grid:")
-println(Y)
-
-f = X^2 + Y^2
-
-using Plots
-
-plot(X, Y, f, st = :surface)
-
-using SymPy, Unitful, Observables
-
-@syms x y 
-
-v = 5*x*u"m/s"
-
-e = 5u"c"
-
-v/e
-
-v = Observable(5*x*u"m/s")
-
-e = 2*v[]
-
-v[] = 10*y*u"m/s"
-
-using ForwardDiff
-
-f = x -> @. x[1]^2 + 2x[2]
-
-function f1(x, y)
-    return x^2 + 2y
-    
-end
-
-f1
-
-ForwardDiff.gradient(f1)
-
-isa(f(x), Real)
-
-df = @. ForwardDiff.derivative(x -> sin(x), x)
-
-using Plots
-
-quiver([0], [0], quiver=([1], [1]))
-
-include("modules.jl")
-using .NumpyJulia
-
-x = y = 0:0.1:1
-X, Y = meshgrid(x, y)
+x  = rand(10000, 10000);
+y  = rand(10000, 10000);
+@benchmark f1(x, y)
+@benchmark f2(x, y)
