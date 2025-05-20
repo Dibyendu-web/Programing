2f82c1ead0178d7e28727d791ba883d9dd5c5a16
Dibyendu-web <rabinghosh52@gmail.com>
2025-05-20 18:38:38 +0530

    Create Gaussianplot.jl

---
+++ Julia/Electrodynamics/Gaussianplot.jl
@@ -0,0 +1,11 @@
+using Plots, LaTeXStrings
+
+gr()
+
+function gauss(x, x0=0, sigma=1)
+    return exp(-((x - x0)^2) / (2 * sigma^2))
+    
+end
+
+plot(x -> gauss(x, 0.3, 0.2), -1, 1, title="Gaussian Function", xlabel=L"x", ylabel=L"f(x)",label = L"f(x) = e^{-\frac{(x - x_0)^2}{2 \sigma^2}}", legend = :topleft)
+plot!(x -> -gauss(x, -0.3, 0.3), -1, 1, title="Gaussian Function", xlabel=L"x", ylabel=L"f(x)", label = L"f(x) = -e^{-\frac{(x - x_0)^2}{2 \sigma^2}}")
