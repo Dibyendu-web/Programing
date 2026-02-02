8e0633da8169e1f251e708dde188344beabb84ec
Dibyendu-web <rabinghosh52@gmail.com>
2026-02-02 14:28:15 +0530

    02022026

--- Julia/Electrodynamics/Gaussianplot.jl
+++ Julia/Electrodynamics/Gaussianplot.jl
@@ -2,10 +2,12 @@ using Plots, LaTeXStrings
 
 gr()
 
-function gauss(x, x0=0, sigma=1)
-    return exp(-((x - x0)^2) / (2 * sigma^2))
+function gauss(x, x0=0, σ=1)  
+    return exp(-((x - x0)^2) / (2 * σ^2))
     
 end
 
-plot(x -> gauss(x, 0.3, 0.2), -1, 1, title="Gaussian Function", xlabel=L"x", ylabel=L"f(x)",label = L"f(x) = e^{-\frac{(x - x_0)^2}{2 \sigma^2}}", legend = :topleft)
-plot!(x -> -gauss(x, -0.3, 0.3), -1, 1, title="Gaussian Function", xlabel=L"x", ylabel=L"f(x)", label = L"f(x) = -e^{-\frac{(x - x_0)^2}{2 \sigma^2}}")
+@time begin
+    plot(x -> gauss(x, 0.3, 0.2), -1, 1, title="Gaussian Function", xlabel=L"x", ylabel=L"f(x)",label = L"f(x) = e^{-\frac{(x - x_0)^2}{2 \sigma^2}}", legend = :topleft)
+    plot!(x -> -gauss(x, -0.3, 0.3), -1, 1, title="Gaussian Function", xlabel=L"x", ylabel=L"f(x)", label = L"f(x) = -e^{-\frac{(x - x_0)^2}{2 \sigma^2}}")
+end
--- Julia/Projects/Osc Copy.jl
+++ Julia/Projects/Osc Copy.jl
@@ -1,18 +1,23 @@
 using PyCall, GLMakie, LaTeXStrings
 
-function connect()
-    eyes = pyimport("eyes17.eyes")
+eyes = pyimport("eyes17.eyes")
 
-    m = eyes.open()
+global m = eyes.open()
 
+if !(m.connected) 
+    println("Waiting for connection...")
+    for i in 1:10
+        sleep(1)
+        println("Attempt $i")
+        global m = eyes.open()
+        if m.connected
+            break
+        end
+    end
     if !(m.connected)
         error("Connection failed")
     end
-
-    return m
 end
-
-
 
 fig = Figure(size = (3840, 2160))
 
@@ -24,7 +29,7 @@ ax = fig[1, 1] = Axis(fig,
     ylabel = L"Y",ygridwidth = 0.5, ygridcolor = :white
 )
 
-GLMakie.xlims!(ax, -16, 16), GLMakie.ylims!(ax, -16, 16)
+xlims!(ax, -16, 16), ylims!(ax, -16, 16)
 vlines!(ax, [0], color = :white, linewidth = 0.5)
 hlines!(ax, [0], color = :white, linewidth = 0.5)
 
@@ -39,7 +44,6 @@ x = Observable(zeros(samples))
 y = Observable(zeros(samples))
 lines!(ax, x, y, label=L"x-y")
 
-m = connect()
 
 function collect()
     tx, xc, ty, yc = m.capture2(samples, 1)
@@ -66,9 +70,8 @@ end
 
 @async while running[]
     update()
-    sleep(0.003)
+    sleep(0.001)
 end
 
 
 fig
-
